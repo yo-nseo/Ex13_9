b5517ea03c372655301bc2a9c21de2d5f2fea967
YoonSeo4618 <w2035@e-mirim.hs.kr>
2021-07-30 11:11:08 +0900

    Edit activity_main.xml add TextView

--- app/src/main/java/kr/hs/emirim/ohyoonseo/ex13_9/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/ohyoonseo/ex13_9/MainActivity.java
@@ -3,12 +3,50 @@ package kr.hs.emirim.ohyoonseo.ex13_9;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
+    SeekBar seek1, seek2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setTitle("Thread 기본");
+        seek1 = findViewById(R.id.seek1);
+        seek2 = findViewById(R.id.seek2);
+        Button btnStart = findViewById(R.id.btn_start);
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Thread thread1 = new Thread() {
+                    @Override
+                    public void run() {
+                        for (int i = seek1.getProgress(); i < 100; i+=2) {
+                            seek1.setProgress(seek1.getProgress() + 2);
+                            SystemClock.sleep(100);
+                        }
+                    }
+                };
+
+                thread1.start();
+
+                Thread thread2 = new Thread() {
+                    @Override
+                    public void run() {
+                        for (int i = seek2.getProgress(); i < 100; i++) {
+                            seek2.setProgress(seek2.getProgress() + 1);
+                            SystemClock.sleep(100);
+                        }
+                    }
+                };
+                thread2.start();
+            }
+        });
     }
 }
